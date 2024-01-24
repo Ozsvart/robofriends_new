6bea3fc7a69475a1eb7af376ee1773cedd949937
OViki <v.ozsvart@gmail.com>
2024-01-24 23:16:50 +0100

    ClassName and Props

--- src/index.js
+++ src/index.js
@@ -4,11 +4,12 @@ import "./index.css";
 import App from "./App";
 import Hello from "./Hello";
 import reportWebVitals from "./reportWebVitals";
+import "tachyons";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Hello />
+    <Hello greeting={"Hello " + "React Ninja"} />
   </React.StrictMode>
 );
 
